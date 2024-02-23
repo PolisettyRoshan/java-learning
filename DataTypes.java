fe3cf4096ccb13c86ca698ff6ccaa6100ef47318
PolisettyRoshan <polisettyroshan@gmail.com>
2024-02-23 21:45:27 +0530

    notes of DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,36 @@
+class DataTypes {
+    public static void main (String[] args) {
+        int negativeNumber = -2;
+        char letter = 's';
+        String name = "Roshan";
+        boolean passOrFail = true;
+        // boolean switch = false;
+
+        int number;
+
+
+        // code
+
+        number = 100;
+        System.out.println(number);
+        System.out.println(negativeNumber);
+        System.out.println(letter);
+        System.out.println(name);
+        System.out.println(passOrFail);
+
+        double decimalNumber = 10.3;
+        System.out.println(decimalNumber);
+
+        float floatingNumber = 15.6f;
+        System.out.println(floatingNumber);
+
+        char nice = 65;
+        System.out.println(nice);
+
+        
+
+
+    }
+
+
+}
