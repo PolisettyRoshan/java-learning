f5d25f523051951df8ca6ef7f6c64421154b8254
PolisettyRoshan <polisettyroshan@gmail.com>
2024-02-23 21:43:08 +0530

    notes oc Logical program

---
+++ LogicalProgram.java
@@ -0,0 +1,15 @@
+class LogicalProgram {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+        int add = number1+number2;
+        int subtractin = number1-number2;
+        int multiplication = number1*number2;
+        double division = (double)number1/number2;
+        System.out.println(add);
+        System.out.println(subtractin);
+        System.out.println(multiplication);
+        System.out.println(division);
+
+    }
+}
