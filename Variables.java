e12a3ef5776c4d0d5720d52cc2ee1d5ec5b8b227
PolisettyRoshan <polisettyroshan@gmail.com>
2024-02-23 21:41:25 +0530

    notes of variables

---
+++ Variables.java
@@ -0,0 +1,17 @@
+class Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello,Variables");
+
+        int x = 10;
+        int y = 20;
+
+        int z = x+y;
+        System.out.println(z);
+
+        int number1 = 20;
+        int number2 = 30;
+
+        int subResultTotal = number1-number2;
+        System.out.println(C);
+    }
+}
